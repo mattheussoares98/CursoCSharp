a1b3ca9875041105defc08632c3d420217024b97
Mattheus Soares <mattheussbarbosa@hotmail.com>
2022-06-08 22:22:28 -0300

    Terminei a parte de DateTime

--- CursoCSharp/API/Datas.cs
+++ CursoCSharp/API/Datas.cs
@@ -7,23 +7,34 @@ using System.Threading.Tasks;
 namespace CursoCSharp.API {
 
     sealed class AlterarData {
-        public AlterarData(double dias) {
-            NovaData = NovaData.AddDays(dias);
-        }
-        //public AlterarData(double meses) {
-        //    NovaData = NovaData.AddDays(meses);
-        //}
-        DateTime NovaData {
-            get; set;
-        }
 
+        public AlterarData() {
+        }
+        public static void AlterarDataInformada(DateTime dataInformada, double dias = 0, int meses = 0, int anos = 0) {
+            dataInformada = dataInformada.AddDays(dias);
+            dataInformada = dataInformada.AddMonths(meses);
+            dataInformada = dataInformada.AddYears(anos);
+
+            Console.WriteLine(dataInformada);
+        }
     }
     internal class Datas {
 
         public static void Executar() {
-            //AdicionarData()
-            //AdicionarMes(dataInformada: DateTime.Now, meses: 1);
-            //AdicionarAno(dataInformada: DateTime.Now, anos: 1);
+            AlterarData.AlterarDataInformada(DateTime.Now, dias: 1, meses: 1, anos: 1);
+            AlterarData.AlterarDataInformada(DateTime.Now, dias: -1, meses: -1, anos: -1);
+
+            Console.WriteLine("\nDate: " + DateTime.Now.Date);
+            Console.WriteLine("Minute: " + DateTime.Now.Minute);
+            Console.WriteLine("Month: " + DateTime.Now.Month);
+            Console.WriteLine("Year: " + DateTime.Now.Year);
+            Console.WriteLine("Day: " + DateTime.Now.Day + "\n");
+
+            Console.WriteLine(DateTime.Now.ToString("d"));
+            Console.WriteLine(DateTime.Now.ToString("dd"));
+            Console.WriteLine(DateTime.Now.ToString("D"));
+            Console.WriteLine(DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")); //quando está usando o toString, só de digitar "d", "m" ou "y", o visual studio já mostra sugestões indicando como fica se usar de formas diferentes
+
         }
     }
 }
